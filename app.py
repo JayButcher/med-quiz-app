996fe8c76f0da4ed107bce93be3b8d5e00a524c0
JayButcher <jpjsantos@gmail.com>
2026-04-25 12:43:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ except:
 genai.configure(api_key=API_KEY)
 
 # Usamos o 1.5 Flash para velocidade e longa janela de contexto (para ler imagem + texto longo)
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-1.5-flash-latest')
 
 st.set_page_config(page_title="MedCase Tutor - UNIFUNCESI", layout="wide")
 
